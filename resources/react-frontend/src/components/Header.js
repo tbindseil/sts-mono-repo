9a086ad6ca4762d8e47cd20d47d1e10e2c9ecb2a
TJ <tjbindseil@gmail.com>
2021-01-13 22:01:48 -0700

    its somethin

--- resources/react-frontend/src/components/Header.js
+++ resources/react-frontend/src/components/Header.js
@@ -3,21 +3,21 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 export const Header = ({screenTitle}) => {
 
-  const sectionStyle = {
+  const navbarStyle = {
     border: '5px solid blue'
   };
 
   return (
     <div className="row justify-content-center">
 
-      <Navbar style={sectionStyle}>
+      <Navbar style={navbarStyle}>
         <Navbar.Collapse>
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about-us">About Us</Nav.Link>
-            <Nav.Link href="/get-involved">Get Involved</Nav.Link>
-            <Nav.Link href="/request-a-tutor">Request A Tutor</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Home</Nav.Link> |
+            <Nav.Link href="/about-us"> About Us</Nav.Link> |
+            <Nav.Link href="/get-involved"> Get Involved</Nav.Link> |
+            <Nav.Link href="/request-a-tutor"> Request A Tutor</Nav.Link> |
+            <Nav.Link href="/contacts"> Contacts</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
