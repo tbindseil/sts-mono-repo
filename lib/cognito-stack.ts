f4b7cba5314eeacb80a681ff72c6fb7530941352
TJ <tjbindseil@gmail.com>
2021-01-24 15:26:50 -0700

    fix indentation

--- lib/cognito-stack.ts
+++ lib/cognito-stack.ts
@@ -2,28 +2,28 @@ import { CfnOutput, Construct, StackProps, Stack } from '@aws-cdk/core';
 import { UserPool, UserPoolClient } from "@aws-cdk/aws-cognito";
 
 export class CognitoStack extends Stack {
-  public readonly userPool: UserPool;
+    public readonly userPool: UserPool;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    this.userPool = new UserPool(this, "UserPool", {
-      selfSignUpEnabled: true, // Allow users to sign up
-      autoVerify: { email: true }, // Verify email addresses by sending a verification code
-      signInAliases: { email: true }, // Set email as an alias
-    });
+        this.userPool = new UserPool(this, "UserPool", {
+            selfSignUpEnabled: true, // Allow users to sign up
+            autoVerify: { email: true }, // Verify email addresses by sending a verification code
+            signInAliases: { email: true }, // Set email as an alias
+        });
 
-    const userPoolClient = new UserPoolClient(this, "UserPoolClient", {
-      userPool: this.userPool,
-      generateSecret: false, // Don't need to generate secret for web app running on browsers
-    });
+        const userPoolClient = new UserPoolClient(this, "UserPoolClient", {
+            userPool: this.userPool,
+            generateSecret: false, // Don't need to generate secret for web app running on browsers
+        });
 
-    // Export values
-    new CfnOutput(this, "UserPoolId", {
-      value: this.userPool.userPoolId,
-    });
-    new CfnOutput(this, "UserPoolClientId", {
-      value: userPoolClient.userPoolClientId,
-    });
-  }
+        // Export values
+        new CfnOutput(this, "UserPoolId", {
+            value: this.userPool.userPoolId,
+        });
+        new CfnOutput(this, "UserPoolClientId", {
+            value: userPoolClient.userPoolClientId,
+        });
+    }
 }
