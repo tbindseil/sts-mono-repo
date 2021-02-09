318c0a7137b6c30a038ee6188663c8c39882a061
kearbey6239 <bradfordkearbey@gmail.com>
2021-02-08 19:17:22 -0600

    just stick top nav bar

--- resources/react-frontend/src/components/Header.js
+++ resources/react-frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo-square.png';
+import logo from '../images/logo-square(1).png';
 import burger from '../images/burger.png';
 import MediaQuery from 'react-responsive'
 import {
@@ -20,25 +20,29 @@ export const Header = ({screenTitle}) => {
 		borderstyle: 'solid',
 		borderRadius: '0px',
    		marginBottom: '0px',
+   		position: 'sticky', 
+   		top: '0',
+   		backgroundColor: 'white',
   	};
   
   	const imgStyle = {
   		width: '25px',
   		height: '25px',
   		marginRight: '0px',
-  		marginTop: '50px',
+  		marginBottom: '2px',
+  		marginTop: '35px',
   		marginLeft: '5px',
 
   	};
 
 return (
 
-	<Navbar style = {navbarStyle}>
+	<Navbar sticky = "bottom"  style = {navbarStyle}>
 
-		<a class = "navLogo"  marginLeft= "0px" marginBottom= "0px"  href="/home"> <img width ="100px" height = "100px"src={logo} alt="logo"/></a>
+		<a class = "navLogo"  marginLeft= "0px" href="/home"> <img width ="100px" height = "52px"src={logo} alt="logo"/></a>
 	
 		<MediaQuery maxWidth={765}>
-
+			 
 			<NavDropdown title= <img style={imgStyle} src={burger} alt="dropdown"/>id="collasible-nav-dropdown">
 				<a class ="dropDown" href="/" >Home  </a>
 	 			<NavDropdown.Divider />
@@ -54,7 +58,7 @@ return (
 
 				<a class ="dropDown" href="/contacts">Contact Us</a>
   			</NavDropdown>
-
+  			
 		</MediaQuery>
 
       	<Navbar.Collapse>
