3136e04fcab316abda484751930c420619c8b928
TJ <tjbindseil@gmail.com>
2021-02-09 19:40:35 -0700

    remove a bit more comments

--- resources/react-frontend/src/components/auth/AnonymousUser.js
+++ resources/react-frontend/src/components/auth/AnonymousUser.js
@@ -1,7 +1,3 @@
-// redirect here when profile is requested, but no logged in user is present
-// this page gives three options, login, register, or confirm
-// each is their own component (and link on this page)
-
 import React from 'react';
 
 import {Header} from '../Header';
