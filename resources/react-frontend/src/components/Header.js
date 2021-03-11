5e7a53933b98b4401cad19e6eeab231068105a0c
TJ <tjbindseil@gmail.com>
2021-03-10 19:34:46 -0700

    clean up header code format and trigger about us drop down on mouse over

--- resources/react-frontend/src/components/Header.js
+++ resources/react-frontend/src/components/Header.js
@@ -1,93 +1,101 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../images/logo-square(1).png';
 import burger from '../images/burger.png';
 import MediaQuery from 'react-responsive'
-import {
-	Navbar,
-	Nav,
-	NavDropdown, 
-	Container,
-} from 'react-bootstrap';
+import { Navbar, NavDropdown } from 'react-bootstrap';
 
-export const Header = ({screenTitle}) => {
+export const Header = () => {
 
-	const navbarStyle = {
-		borderTopWidth: '0px',
-		borderRightWidth: '0px',
-		borderLeftWidth: '0px',
-		borderBottomWidth: '10px',
-		borderColor: '#005D8c',
-		borderstyle: 'solid',
-		borderRadius: '0px',
-   		marginBottom: '0px',
-   		position: 'sticky', 
-   		top: '0',
-   		backgroundColor: 'white',
-  	};
-  
-  	const imgStyle = {
-  		width: '25px',
-  		height: '25px',
-  		marginRight: '0px',
-  		marginBottom: '2px',
-  		marginTop: '35px',
-  		marginLeft: '5px',
+    const navbarStyle = {
+        borderTopWidth: '0px',
+        borderRightWidth: '0px',
+        borderLeftWidth: '0px',
+        borderBottomWidth: '10px',
+        borderColor: '#005D8c',
+        borderstyle: 'solid',
+        borderRadius: '0px',
+        marginBottom: '0px',
+        position: 'sticky',
+        top: '0',
+        backgroundColor: 'white',
+    };
 
-  	};
+    const imgStyle = {
+        width: '25px',
+        height: '25px',
+        marginRight: '0px',
+        marginBottom: '2px',
+        marginTop: '35px',
+        marginLeft: '5px',
+    };
 
-return (
+    const [show, setShow] = useState(false);
+    const showAboutUsDropdown = () => {
+        setShow(true);
+    }
+    const hideAboutUsDropdown = () => {
+        setShow(false);
+    }
 
-	<Navbar sticky = "bottom"  style = {navbarStyle}>
+    return (
 
-		<a class = "navLogo"  marginLeft= "0px" href="/home"> <img width ="100px" height = "52px"src={logo} alt="logo"/></a>
-	
-		<MediaQuery maxWidth={765}>
-			 
-			<NavDropdown title= <img style={imgStyle} src={burger} alt="dropdown"/>id="collasible-nav-dropdown">
-				<a class ="dropDown" href="/" >Home  </a>
-	 			<NavDropdown.Divider />
+        <Navbar sticky="bottom"  style={navbarStyle}>
 
-				<a class ="dropDown" href="/about-us" >About STS </a>
-				<NavDropdown.Divider />
+            <a class="navLogo"  marginLeft="0px" href="/home"> <img width="100px" height="52px"src={logo} alt="logo"/></a>
 
-				<a class ="dropDown" href="/process" >Our Process </a>
-				<NavDropdown.Divider />
+            <MediaQuery maxWidth={765}>
 
-				<a class ="dropDown" href="/get-involved">Become A Tutor </a>		
-	 			<NavDropdown.Divider />
+                <NavDropdown title={<img style={imgStyle} src={burger} alt="dropdown"/>} id="collasible-nav-dropdown">
 
-	 			<a class ="dropDown" href="/request-a-tutor" >Request A Tutor  </a>
-				<NavDropdown.Divider />
+                    <a class="dropDown" href="/" >Home  </a>
+                    <NavDropdown.Divider />
 
-				<a class ="dropDown" href="/contacts">Contact Us</a>
-  			</NavDropdown>
-  			
-		</MediaQuery>
+                    <a class="dropDown" href="/about-us" >About STS </a>
+                    <NavDropdown.Divider />
 
-      	<Navbar.Collapse>
+                    <a class="dropDown" href="/process" >Our Process </a>
+                    <NavDropdown.Divider />
 
-			<a class ="navBar" href="/contacts"> Contact Us </a>
-		
-			<a class ="navBar" href="/request-a-tutor" >Request A Tutor  </a>
-				
-			<a class ="navBar" href="/get-involved">Become A Tutor </a>	
+                    <a class="dropDown" href="/get-involved">Become A Tutor </a>
+                    <NavDropdown.Divider />
 
-			<a class ="navBar">
+                    <a class="dropDown" href="/request-a-tutor" >Request A Tutor  </a>
+                    <NavDropdown.Divider />
 
-			<NavDropdown id="collasible-nav-dropdown" drop = "down" key = "down" title =  <a class = "navBarA" href="/about-us">About Us <i class="arrow down"></i></a>	>
-			
-      	    <a class ="navBar2" href="/about-us" >About STS </a>
-      	    <a class ="navBar2" href="/process" >Our Process  </a>
-      	    </NavDropdown>
+                    <a class="dropDown" href="/contacts">Contact Us   </a>
+                    <NavDropdown.Divider />
 
-			</a>
+                </NavDropdown>
 
-		    <a class ="navBar" href="/" >Home  </a>
-				
-     	</Navbar.Collapse>
-	  
-    </Navbar>
+            </MediaQuery>
 
-);
+            <Navbar.Collapse>
+
+                <a class="navBar" href="/contacts"> Contact Us </a>
+
+                <a class="navBar" href="/request-a-tutor" >Request A Tutor  </a>
+
+                <a class="navBar" href="/get-involved">Become A Tutor </a>
+
+                <a class="navBar" href="/about-us" onMouseEnter={showAboutUsDropdown} onMouseLeave={hideAboutUsDropdown}>
+
+                    <NavDropdown
+                        id="collasible-nav-dropdown"
+                        drop="down"
+                        key="down"
+                        show={show}
+                        title={<a class="navBarA" href="/about-us">About Us <i class="arrow down"></i></a>}>
+                        <a class ="navBar2" href="/about-us" >About STS </a>
+                        <a class ="navBar2" href="/process" >Our Process  </a>
+                    </NavDropdown>
+
+                </a>
+
+                <a class="navBar" href="/" >Home  </a>
+
+            </Navbar.Collapse>
+
+        </Navbar>
+
+    );
 };
-
