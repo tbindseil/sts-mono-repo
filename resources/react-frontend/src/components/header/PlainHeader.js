56e83dc5c8438eafbcf9db190fe6fa216fee01f6
TJ <tjbindseil@gmail.com>
2021-03-12 18:20:12 -0700

    layout wide view header matches without React Bootstrap stuff

--- resources/react-frontend/src/components/header/PlainHeader.js
+++ resources/react-frontend/src/components/header/PlainHeader.js
@@ -8,29 +8,21 @@ export function PlainHeader() {
         <ul className="NavBar">
             <a className="navLogo"  marginLeft="0px" href="/home"> <img width="100px" height="52px"src={logo} alt="logo"/></a>
 
-            <li className="NavBarItem">
-                <a href="/contacts">Contact Us</a>
-            </li>
+            <a className="NavBarItem" href="/contacts">Contact Us</a>
 
-            <li className="NavBarItem">
-                <a href="/request-a-tutor">Request A Tutor</a>
-            </li>
+            <a className="NavBarItem" href="/request-a-tutor">Request A Tutor</a>
 
-            <li className="NavBarItem">
-                <a href="/get-involved">Become A Tutor</a>
-            </li>
+            <a className="NavBarItem" href="/get-involved">Become A Tutor</a>
 
             <div className="DropDown RightAlign">
-                <button className="NavBarItem DropBtn">About Us</button>
+                <button className="NavBarItem DropBtn">About Us  <i className="arrow down"/></button>
                 <div className="DropDownContent">
                     <a href="/about-us">About STS</a>
                     <a href="/process">Our Process</a>
                 </div>
             </div>
 
-            <li className="NavBarItem">
-                <a href="/">Home</a>
-            </li>
+            <a className="NavBarItem" href="/">Home</a>
         </ul>
     );
 }
