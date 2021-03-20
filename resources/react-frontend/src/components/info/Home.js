aff75d09adb59286810caf93c3ab511f2d090ba1
TJ <tjbindseil@gmail.com>
2021-03-20 10:40:35 -0600

    wip - dried out Home

--- resources/react-frontend/src/components/info/Home.js
+++ resources/react-frontend/src/components/info/Home.js
@@ -4,18 +4,6 @@ import stock_photo from '../../images/stock_photo.jpg';
 import MediaQuery from 'react-responsive'
 import {Bottom} from '../Bottom';
 
-const modo = {
-  textAlign: 'center',
-  fontFamily: 'Arial',
-  fontVariant: 'small-caps',
-  fontWeight: 'bold',
-  color: '#005D8c',
-}
-
-const style = {
-    height: '100%'
-}
-
 export const Home = () => (
 
     <div className="TopLevelContainer">
@@ -26,39 +14,41 @@ export const Home = () => (
 
         <MediaQuery minWidth={765}>
 
-            <header className="PageBorder">
+            <HomeBody
+                pageBorderClass={"PageBorder"}
+                underlineClass={"UnderLine"}/>
 
-                <h1 className= "titleMain">
-                    Students Teaching Students
-                </h1>
-
-                <hr className="UnderLine"/>
-
-                <p style= {modo}>
-                    Enhancing education through student to student tutoring
-                </p>
-
-            </header>
         </MediaQuery>
 
         <MediaQuery maxWidth={765}>
 
-            <header className={"PageBorder2"}>
+            <HomeBody
+                pageBorderClass={"PageBorder2"}
+                underlineClass={"UnderLine2"}/>
 
-                <h1 className= "titleMain">
-                    Students Teaching Students
-                </h1>
-
-                <hr className="UnderLine2"/>
-
-                <p style= {modo}>
-                    Enhancing education through student to student tutoring
-                </p>
-
-            </header>
         </MediaQuery>
 
         <Bottom/>
 
     </div>
 );
+
+function HomeBody(props) {
+    return (
+        <>
+            <header className={props.pageBorderClass}>
+
+                <h1 className="titleMain">
+                    Students Teaching Students
+                </h1>
+
+                <hr className={props.underlineClass}/>
+
+                <p className="Modo">
+                    Enhancing education through student to student tutoring
+                </p>
+
+            </header>
+        </>
+    );
+}
