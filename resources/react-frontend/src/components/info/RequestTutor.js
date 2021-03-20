8408ccc13fa86dfbf1cd966eae2a3e6fdccf0167
TJ <tjbindseil@gmail.com>
2021-03-20 11:25:12 -0600

    dry out request tutor

--- resources/react-frontend/src/components/info/RequestTutor.js
+++ resources/react-frontend/src/components/info/RequestTutor.js
@@ -11,72 +11,24 @@ export const RequestTutor = () => (
 
         <MediaQuery minWidth={765}>
 
-            <header className="PageBorder">
+            <RequestTutorBody
+                pageBorderClass={"PageBorder"}
+                underlineClass={"UnderLine"}
+                textClass={"mainText"}
+                iframeWidth={"700"}
+                iframeTitle={"getInvolvedBigScreenIFrame"}/>
 
-                <h2 className="PageHeader">
-                    Request A Tutor
-                </h2>
-
-                <hr className="UnderLine"/>
-
-                <p className= "mainText">
-                    Students Teaching Students is a nonprofit tutoring organization that pairs students in their own area for free tutoring services.
-                </p>
-                <p className= "mainText">
-                    If you are looking for help with test preperation, homework, or general subject matter, request a tutor for your subjects of interest and state you availability in the form below.
-                </p>
-
-                <hr className="UnderLine"/>
-
-                <p className="FormStyle">
-                    <iframe
-                        src="https://docs.google.com/forms/d/e/1FAIpQLSf4uPCMvxLew5migtPBZwiavpJRrS24SdE9XbYlScTLzEpdeQ/viewform?embedded=true"
-                        width="700"
-                        height="520"
-                        frameborder="0"
-                        marginheight="0"
-                        marginwidth="0"
-                        title="getInvolvedBigScreenIFrame">
-                        Loading…
-                    </iframe>
-                </p>
-
-            </header>
         </MediaQuery>
 
         <MediaQuery maxWidth={765}>
 
-            <header className="PageBorder2">
+            <RequestTutorBody
+                pageBorderClass={"PageBorder2"}
+                underlineClass={"UnderLine2"}
+                textClass={"mainText2"}
+                iframeWidth={"100%"}
+                iframeTitle={"getInvolvedSmallScreenIFrame"}/>
 
-                <h2 className="PageHeader">
-                    Request A Tutor
-                </h2>
-
-                <hr className="UnderLine2"/>
-
-                <p className= "mainText2">
-                    Students Teaching Students is a nonprofit tutoring organization that pairs students in their own area for free tutoring services.
-                </p>
-                <p className="mainText2">
-                    If you are looking for help with test preperation, homework, or general subject matter, request a tutor for your subjects of interest and state you availability in the form below.
-                </p>
-
-                <hr className="UnderLine2"/>
-
-                <p className="FormStyle">
-                    <iframe
-                        src="https://docs.google.com/forms/d/e/1FAIpQLSf4uPCMvxLew5migtPBZwiavpJRrS24SdE9XbYlScTLzEpdeQ/viewform?embedded=true"
-                        width="100%"
-                        height="520"
-                        frameborder="0"
-                        marginheight="0"
-                        marginwidth="0"
-                        title="getInvolvedSmallScreenIFrame">
-                        Loading…
-                    </iframe>
-                </p>
-
-            </header>
         </MediaQuery>
 
         <Bottom/>
@@ -84,3 +36,41 @@ export const RequestTutor = () => (
     </div>
 
 );
+
+function RequestTutorBody(props) {
+    return (
+        <>
+            <header className={props.pageBorderClass}>
+
+                <h2 className="PageHeader">
+                    Request A Tutor
+                </h2>
+
+                <hr className={props.underlineClass}/>
+
+                <p className={props.textClass}>
+                    Students Teaching Students is a nonprofit tutoring organization that pairs students in their own area for free tutoring services.
+                </p>
+                <p className={props.textClass}>
+                    If you are looking for help with test preperation, homework, or general subject matter, request a tutor for your subjects of interest and state you availability in the form below.
+                </p>
+
+                <hr className={props.underlineClass}/>
+
+                <p className="FormStyle">
+                    <iframe
+                        src="https://docs.google.com/forms/d/e/1FAIpQLSf4uPCMvxLew5migtPBZwiavpJRrS24SdE9XbYlScTLzEpdeQ/viewform?embedded=true"
+                        width={props.iframeWidth}
+                        height={"520"}
+                        frameborder="0"
+                        marginheight="0"
+                        marginwidth="0"
+                        title={props.iframeTitle}>
+                        Loading…
+                    </iframe>
+                </p>
+
+            </header>
+        </>
+    );
+}
