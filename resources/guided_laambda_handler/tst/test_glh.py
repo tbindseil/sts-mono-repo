9b0d5e23c8a4155ef8bce496fbe414b56e6b6996
TJ <tjbindseil@gmail.com>
2021-04-21 20:44:55 -0600

    the rest of the tests pass

--- resources/guided_laambda_handler/tst/test_glh.py
+++ resources/guided_laambda_handler/tst/test_glh.py
@@ -27,7 +27,7 @@ class TestGLH(unittest.TestCase):
         self.mock_on_handle = MagicMock()
         self.glh = GLH(self.mock_translate_input, self.mock_translate_output, self.mock_on_handle)
 
-    def test_input_is_translated(self, mock_get_database_engine, mock_session_maker):
+    def test_input_is_translated_and_returned(self, mock_get_database_engine, mock_session_maker):
         event = "event"
         context = "context"
         input = "input translated"
@@ -56,78 +56,57 @@ class TestGLH(unittest.TestCase):
 
         self.assertEqual(actual_response, expected_response)
 
-    def est_handler_returns_500_on_general_exception(self, mock_get_database_engine, mock_session_maker):
-        self.magic_mock_method.side_effect = Exception
+    def test_handler_returns_500_on_general_exception(self, mock_get_database_engine, mock_session_maker):
+        self.mock_on_handle.side_effect = Exception
 
-        event = { 'httpMethod': self.http_method }
-        context = "context"
-        response = self.glh.handle(event, context)
+        actual_response = self.glh.handle('event', 'context')
+        expected_response = response_factory(500, 'service error')
 
-        self.assertEqual(response['statusCode'], 500)
+        self.assertEqual(actual_response, expected_response)
 
-    def est_session_is_closed_on_success(self, mock_get_database_engine, mock_session_maker):
+    def test_session_is_closed_on_success(self, mock_get_database_engine, mock_session_maker):
         mock_Session = MagicMock()
         mock_session = MagicMock()
         mock_session_maker.return_value = mock_Session
         mock_Session.return_value = mock_session
 
-        response_code = 200
-        response_body = "body"
-        self.magic_mock_method.return_value = response_code, response_body
-
-        event = { 'httpMethod': self.http_method }
-        context = "context"
-        actual_response = self.glh.handle(event, context)
+        self.mock_translate_input.return_value = 200, 'body'
+        self.glh.handle('event', 'context')
         mock_session.close.assert_called_once()
 
         mock_session.reset_mock()
 
-        self.magic_mock_method.side_effect = Exception
-        actual_response = self.glh.handle(event, context)
+        self.mock_on_handle.side_effect = Exception
+        self.glh.handle('event', 'context')
         mock_session.close.assert_called_once()
 
         mock_session.reset_mock()
 
-        self.magic_mock_method.side_effect = AuthException
-        actual_response = self.glh.handle(event, context)
+        self.mock_on_handle.side_effect = AuthException
+        self.glh.handle('event', 'context')
         mock_session.close.assert_called_once()
 
-
-    def est_response_code_and_body_are_returned_when_successful(self, mock_get_database_engine, mock_session_maker):
-        response_code = 200
-        response_body = "body"
-        expected_response_body = json.dumps(response_body)
-        self.magic_mock_method.return_value = response_code, response_body
-
-        event = { 'httpMethod': self.http_method }
-        context = "context"
-        actual_response = self.glh.handle(event, context)
-        self.assertEqual(response_code, actual_response['statusCode'])
-        self.assertEqual(expected_response_body, actual_response['body'])
-
-    def est_session_is_committed_after_strategy_called_successful(self, mock_get_database_engine, mock_session_maker):
+    def test_session_is_committed_after_on_handle_called_successful(self, mock_get_database_engine, mock_session_maker):
         mock_Session = MagicMock()
         mock_session = MagicMock()
         mock_session_maker.return_value = mock_Session
         mock_Session.return_value = mock_session
 
-        response_code = 200
-        response_body = "body"
-        self.magic_mock_method.return_value = response_code, response_body
+        self.mock_translate_output.return_value = 200, "body"
 
         event = { 'httpMethod': self.http_method }
         context = "context"
         actual_response = self.glh.handle(event, context)
         mock_session.commit.assert_called_once()
 
-    def est_get_claims_from_event_throws_when_token_not_present(self, mock_get_database_engine, mock_session_maker):
+    def test_get_claims_from_event_throws_when_token_not_present(self, mock_get_database_engine, mock_session_maker):
         event = {}
 
         with self.assertRaises(AuthException) as e:
             get_claims_from_event(event)
 
 
-    def est_get_claims_from_event_throws_when_get_and_verify_claims_throws(self, mock_get_database_engine, mock_session_maker):
+    def test_get_claims_from_event_throws_when_get_and_verify_claims_throws(self, mock_get_database_engine, mock_session_maker):
         event = {'headers': {'Authorization': 'auth_token'}}
 
         with patch('authentication_validation.cognito_validation.get_and_verify_claims') as mock_get_and_verify_claims:
@@ -135,7 +114,7 @@ class TestGLH(unittest.TestCase):
             with self.assertRaises(AuthException) as e:
                 get_claims_from_event(event)
 
-    def est_get_claims_from_event_returns_token_when_successful(self, mock_get_database_engine, mock_session_maker):
+    def test_get_claims_from_event_returns_token_when_successful(self, mock_get_database_engine, mock_session_maker):
         event = {'headers': {'Authorization': 'auth_token'}}
 
         expected_result = "token"
