df84a32d2fb9bebce2c74e5d65c4032d78e9bed4
TJ <tjbindseil@gmail.com>
2021-04-21 21:37:58 -0600

    write avail lambda function

--- resources/availability-lambda/lambda_function.py
+++ resources/availability-lambda/lambda_function.py
@@ -1,39 +1,76 @@
 from datetime import datetime
 
-from guided_lambda_handler.guided_lambda_handler import GuidedLambdaHandler, AuthException, json_to_model, model_to_json, model_list_to_json
+from guided_lambda_handler.guided_lambda_handler import GuidedLambdaHandler, AuthException, json_to_model, model_to_json, model_list_to_json, response_factory
 from models.user import User
 from models.availability import Availability
 
 
+def get_input_translator(event, context)
+    return event["body"]["cognitoId"]
+
+
 # TODO accept date range
-def get_handler(event, context, session, get_claims):
+def get_handler(input, session, get_claims):
+    cognito_id = input
+
     claims = get_claims()
-    cognito_id = claims["cognito:username"]
+    claimed_cognito_id = claims["cognito:username"]
+
+    # ultimately, I'll need a service to validate who can see whose avails..
+    # for now one can only see themselves
+    if claimed_cognito_id != cognito_id:
+        raise AuthException
+
     user = session.query(User).filter(User.cognitoId==cognito_id).one()
+    return user.availabilities
 
-    availabilities_json = model_list_to_json(user.availabilities)
 
-    return 200, availabilities_json
+def post_output_translator(raw_output)
+    availabilities = raw_output
+
+    response = {}
+    for avail in availabilities:
+        response[avail.id] = {
+            'subjects': avail.subjects,
+            'startTime': avail.startTime,
+            'endTime': avail.endTime,
+            'tutor': avail.tutor,
+        }
+
+    # TODO get rid of datetimejson crap
+    return 200, json.dumps(response)
+
+
+def post_input_translator(event, context)
+    return json_to_model(event["body"], Availability)
 
 
 # TODO make sure there are no overlapping availabilties
-def post_handler(event, context, session, get_claims):
+def post_handler(input, session, get_claims):
+    posted_availability = input
+
     claims = get_claims()
     cognito_id = claims["cognito:username"]
     user = session.query(User).filter(User.cognitoId==cognito_id).one()
-
-    posted_availability = json_to_model(event["body"], Availability)
 
     user.availabilities.append(posted_availability)
     session.add(user)
 
-    return 200, "success"
+    return "success"
+
+def post_output_translator(raw_output)
+    return 200, json.dumps(raw_output)
 
 
-def delete_handler(event, context, session, get_claims):
+def delete_input_translator(event, context)
+    return event['path'].split('/')[-1]
+
+
+def delete_handler(input, session, get_claims):
+    availability_id_to_delete = input
+
     claims = get_claims()
     cognito_id = claims["cognito:username"]
-    availability_id_to_delete = event['path'].split('/')[-1]
     avail_to_delete = session.query(Availability).filter(Availability.id==availability_id_to_delete).one()
 
     if cognito_id != avail_to_delete.tutor:
@@ -41,6 +78,10 @@ def delete_handler(event, context, session, get_claims):
 
     session.delete(avail_to_delete)
 
+    return "success"
+
+
+def delete_output_translator(raw_output)
     return 200, "success"
 
 
@@ -52,11 +93,14 @@ def lambda_handler(event, context):
     for others, return invalid method
     """
 
-    http_method_strategies = {
-            "GET": get_handler,
-            "POST": post_handler,
-            "DELETE": delete_handler,
-    }
-    guided_lambda_handler = GuidedLambdaHandler(http_method_strategies)
-
-    return guided_lambda_handler.handle(event, context)
+    if event["method"] == "GET":
+        get_glh = GLH(get_input_translator, get_handler, get_output_translator)
+        return glh.handle(event, context)
+    elif event["method"] == "POST":
+        post_glh = GLH(post_input_translator, post_handler, post_output_translator)
+        return glh.handle(event, context)
+    elif event["method"] == "DELETE":
+        delete_glh = GLH(delete_input_translator, delete_handler, delete_output_translator)
+        return glh.handle(event, context)
+    else:
+        return response_factory(405, json.dumps("only GET, PUT, and DELETE are valid"))
