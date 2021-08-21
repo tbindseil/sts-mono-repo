fa46e8558baab08f0b1eefb7f65bc65db878bac1
TJ <tjbindseil@gmail.com>
2021-08-20 23:51:31 -0600

    ok, got a lot of ideas and not a lot of stuff, but first i must go back in time and make avails a map so that i can get it quick after getting all requests

--- resources/react-frontend/src/components/calendar/SelectAvailabilityScreen.js
+++ resources/react-frontend/src/components/calendar/SelectAvailabilityScreen.js
@@ -38,7 +38,7 @@ export function SelectAvailabilityScreen(props) {
 // TODO cool idea, allow start time and subject to be selectable here
 function CreateAvailabilityBody(props) {
     const availabilityLambdaUrl = 'https://k2ajudwpt0.execute-api.us-west-2.amazonaws.com/prod'
-    // const availabilityRequestLambdaUrl = 'https://04c0w1j888.execute-api.us-west-2.amazonaws.com/prod/';
+    const availabilityRequestLambdaUrl = 'https://04c0w1j888.execute-api.us-west-2.amazonaws.com/prod/';
     const history = useHistory();
 
     const stateProps = props.location.state;
@@ -68,7 +68,6 @@ function CreateAvailabilityBody(props) {
             }
 
             // startTime and endTime are 12:00:00 am of sunday morning and 11:59:59 of saturday night for week of selectedDate
-            // could be improved by only getting for 1 day for small screen
             const requestStartTime = moment(startTime).toDate();
             const requestEndTime = moment(startTime).add('minute', 30).toDate();
             const url = new URL(availabilityLambdaUrl)
@@ -103,8 +102,78 @@ function CreateAvailabilityBody(props) {
                             tutor: avail.tutor,
                             id: id
                         });
+
+                        const availRequestsUrl = new URL(availabilityRequestLambdaUrl)
+                        const getAvailRequestInput = {
+                            forAvailability: id,
+                            forUser: "",
+                            fromUser: ""
+                        };
+                        availRequestsUrl.searchParams.append('getAvailInput', JSON.stringify(getAvailInput));
+
+                        fetch(availRequestsUrl, {
+                            method: 'GET',
+                            mode: 'cors',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': tokenString
+                            },
+                        })
+                            .then(res => res.json())
+                            .then((result) => {
+
+                            },
+                            (error) => {
+                                setFailed(true);
+                                var message = "1Error getting availabilty requests";
+                                if (error.message) {
+                                    message += ": " + error.message;
+                                }
+                                setErrorMessage(message);
+                            })
+                        .catch(err => { // TODO this code is wet as fuck
+                            setFailed(true);
+                            var message = "2Error getting availabilty requests";
+                            if (err.message) {
+                                message += ": " + err.message;
+                            }
+                            setErrorMessage(message);
+                        });
+
+                        // I'm sorry future tj
+                        // I really need to pull out the fetching logic
+                        //      I shjould look into the service functionality that I saw early in te project
+
+                        // const availRequestsPromise = {
+                            // availId: id,
+                            // promise: 
+
+                        // send a request to get requests given this avails id
+                        // save the request (a promise i think?) to a list
+
                     }
                     setAvailabilities(availabilitiesWithDates);
+
+                    // at the end, wait for all request promises
+                    // for each avail, check all requests and see if status?
+                    // why is the front end doing that when it is a pain in the assssss
+                    // what would the lambda look like?
+                    //
+                    // I would add to the avail lambda
+                    //     such that there is a new resource (think cdk)
+                    //     that resource is, <url/prod>/status/<id> and a GET call
+                    //     then it just calcs the status by seeing if there are any
+                    //     avail requests with... why do i even need the status?
+                    //      well, i either need to filter in the get requests api, or show
+                    //      that some are non open anymore..
+                    //      fuck man, both are rough
+                    //     req statues => ['REQUESTED', 'ACCEPTED', 'DENIED', 'CANCELED']
+                    //     avail statuses => [OPEN, REQUESTED, DENIED, ACCEPTED]
+                    //          OPEN => no request has been accepted, no requst from user
+                    //          REQUESTED => request from user, not accepted or denied
+                    //          DENIED => request from user, denied by tutor
+                    //          ACCEPTED => request from user, accepted by tutor
+
                 },
                     // Note: it's important to handle errors here
                     // instead of a catch() block so that we don't swallow
@@ -143,12 +212,6 @@ function CreateAvailabilityBody(props) {
             }
         });
     };
-
-
-    // now show availabilities
-    // for each avail, we want to show
-    // start | end | tutor | subjects | status | button
-
 
     return (
         <header className={props.pageBorderClass}>
