b5295fa774d23a2eb062afd3f4999c9e403a1662
TJ <tjbindseil@gmail.com>
2021-08-28 19:34:56 -0600

    write some status update code, but realize i have to switch to a map for avails, so switch to map for avails

--- resources/react-frontend/src/components/calendar/SelectAvailabilityScreen.js
+++ resources/react-frontend/src/components/calendar/SelectAvailabilityScreen.js
@@ -93,127 +93,17 @@ function CreateAvailabilityBody(props) {
             })
                 .then(res => res.json())
                 .then((result) => {
-                    const availabilitiesWithDates = []
+                    const availabilitiesWithDates = new Map();
                     for (const [id, avail] of Object.entries(result)) {
-                        availabilitiesWithDates.push({
-                            id: {
-                                endTime: moment.utc(avail.endTime).local().toDate(),
-                                startTime: moment.utc(avail.startTime).local().toDate(),
-                                subjects: avail.subjects,
-                                tutor: avail.tutor,
-                            }
+                        availabilitiesWithDates.set(id, {
+                            id: id,
+                            endTime: moment.utc(avail.endTime).local().toDate(),
+                            startTime: moment.utc(avail.startTime).local().toDate(),
+                            subjects: avail.subjects,
+                            tutor: avail.tutor,
                         });
-
-                        const availRequestsUrl = new URL(availabilityRequestLambdaUrl)
-                        const getAvailRequestInput = {
-                            forAvailability: id,
-                            forUser: "",
-                            fromUser: ""
-                        };
-                        availRequestsUrl.searchParams.append('getAvailInput', JSON.stringify(getAvailInput));
-
-                        fetch(availRequestsUrl, {
-                            method: 'GET',
-                            mode: 'cors',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': tokenString
-                            },
-                        })
-                            .then(res => res.json())
-                            .then((result) => {
-
-                            },
-                            (error) => {
-                                setFailed(true);
-                                var message = "1Error getting availabilty requests";
-                                if (error.message) {
-                                    message += ": " + error.message;
-                                }
-                                setErrorMessage(message);
-                            })
-                        .catch(err => { // TODO this code is wet as fuck
-                            setFailed(true);
-                            var message = "2Error getting availabilty requests";
-                            if (err.message) {
-                                message += ": " + err.message;
-                            }
-                            setErrorMessage(message);
-                        });
-
-                        // I'm sorry future tj
-                        // I really need to pull out the fetching logic
-                        //      I shjould look into the service functionality that I saw early in te project
-
-                        // const availRequestsPromise = {
-                            // availId: id,
-                            // promise: 
-
-                        // send a request to get requests given this avails id
-                        // save the request (a promise i think?) to a list
-
-                        // honestly, (this is probably written somewhere else)
-                        // I think its not needed to know things about every request
-                        // and the only requests we need to know about are the requests from the user
-                        // we need to know about these requests in order to know if the user can send a request or cancel or see pending?
-                        //
-                        // and this is dependent upon the status of the request relative to the user
-                        // all the more reason to offload this to the backend (which is why I think its probably already written)
-                        //
-                        // What I don't know, is why its better offloaded to backend?
-                        // and I don't really care
                     }
                     setAvailabilities(availabilitiesWithDates);
-
-                    // at the end, wait for all request promises
-                    // for each avail, check all requests and see if status?
-                    // why is the front end doing that when it is a pain in the assssss
-                    // what would the lambda look like?
-                    //
-                    // I would add to the avail lambda
-                    //     such that there is a new resource (think cdk)
-                    //     that resource is, <url/prod>/status/<id> and a GET call
-                    //     then it just calcs the status by seeing if there are any
-                    //     avail requests with... why do i even need the status?
-                    //      well, i either need to filter in the get requests api, or show
-                    //      that some are non open anymore..
-                    //      fuck man, both are rough
-                    //     req statues => ['REQUESTED', 'ACCEPTED', 'DENIED', 'CANCELED']
-                    //     avail statuses => [OPEN, REQUESTED, DENIED, ACCEPTED]
-                    //          OPEN => no request has been accepted, no requst from user
-                    //          REQUESTED => request from user, not accepted or denied
-                    //          DENIED => request from user, denied by tutor
-                    //          ACCEPTED => request from user, accepted by tutor
-                    //          CLOSED => no request from user, but one accepted from another user
-                    //          
-                    //     and these avail statuses are relavent to the user
-
-                    // current hangup:
-                    // when we fetch all requests, we could find some that will ultimately evaluate to CLOSED
-                    // so in order to deal with that,
-                    // show all with loading arrow circle thing
-                    // as a request comes back, replace loading arrow circle thing with status
-                    // unless it is closed, in which case have it disappear, or move to bottom of list in special closed section
-                    //
-                    // this also applies to calendar
-                    // unless api the calendar calls has a join command and checks for request with accepted
-
-                    // what is the heart of this problem?
-                    // 1) users need to see if a particular availability exists (? do they? or do they need to request and we tell them?? IDFK?)
-                    // 2) users need to request a particular availability
-                    //
-                    // I actually think souping up get avails sql query could work pretty well so that it only gets stuff calced to have non closed
-                    //
-                    // ok so last question then,
-                    // why does it require a souped up command and not just keep 
-                    //
-                    //
-                    // sorry another question came up.. why not just show the denied ones?
-                    // thats fine except for the count. the count sucks because i think i want to show possible
-                    //
-                    // so why not include denied in the count?
-                    // whats the easiest?
-                    // show denied
                 },
                     // Note: it's important to handle errors here
                     // instead of a catch() block so that we don't swallow
@@ -238,11 +128,65 @@ function CreateAvailabilityBody(props) {
         [startTime, subject]
     );
 
+    /*const getStatuses = useCallback(
+        (availabilities) => {
+            if (!user) {
+                return;
+            }
+
+            console.log("getStatuses");
+
+            availabilities.forEach(avail => {
+                const url = `${availabilityLambdaUrl}/status/${avail.id}`;
+                const tokenString = 'Bearer ' + user.signInUserSession.idToken.jwtToken;
+                fetch(url, {
+                    method: 'GET',
+                    mode: 'cors',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': tokenString
+                    },
+                })
+                    .then(res => res.json())
+                    .then((result) => {
+                        // console.log("can i bind the id?"); no..
+
+                    },
+                        // Note: it's important to handle errors here
+                        // instead of a catch() block so that we don't swallow
+                        // exceptions from actual bugs in components.
+                        (error) => {
+                            setFailed(true);
+                            var message = "1Error getting availabilties";
+                            if (error.message) {
+                                message += ": " + error.message;
+                            }
+                            setErrorMessage(message);
+                        })
+                    .catch(err => { // TODO this code is wet as fuck
+                        setFailed(true);
+                        var message = "2Error getting availabilties";
+                        if (err.message) {
+                            message += ": " + err.message;
+                        }
+                        setErrorMessage(message);
+                    });
+            });
+        },
+        [user]
+    );*/
+
     useEffect(() => {
         getAvailabilities(user);
     }, [
         user, getAvailabilities
     ]);
+
+    /*useEffect(() => {
+        getStatuses(availabilities);
+    }, [
+        availabilities, getStatuses
+    ]);*/
 
     const onCancel = () => {
         history.push({
@@ -283,20 +227,20 @@ function CreateAvailabilityBody(props) {
                 </tr>
 
                 {
-                    availabilities.map(avail => {
+                    Array.from(availabilities.entries()).map(availEntry => {
                         return (
                             <tr>
                                 <td>
-                                    {moment(avail.startTime).format("LT")}
+                                    {moment(availEntry[1].startTime).format("LT")}
                                 </td>
                                 <td>
-                                    {moment(avail.endTime).format("LT")}
+                                    {moment(availEntry[1].endTime).format("LT")}
                                 </td>
                                 <td>
-                                    {avail.tutor}
+                                    {availEntry[1].tutor}
                                 </td>
                                 <td>
-                                    {avail.subjects}
+                                    {availEntry[1].subjects}
                                 </td>
                                 <td>
                                     Status - TODO
