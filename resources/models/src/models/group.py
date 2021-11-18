798f5046339aabda5efcc515dffa5b8812a54a15
TJ <tjbindseil@gmail.com>
2021-11-18 11:21:20 -0700

    first draft of group models

---
+++ resources/models/src/models/group.py
@@ -0,0 +1,29 @@
+from . import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+# Availability class is used, so we must define it here or else it has to be definied by clients of the Group class,
+# and they don't necessarily know that they need to do this
+from .user import User
+from .availability import Availability
+
+
+class Group(Base):
+    """ Group Model for giving users control over who they are exposed to """
+    __tablename__ = "group"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(511), nullable=False)
+
+    parentGroup = Column(Integer, ForeignKey('group.id'), nullable=True) # only null for OriginGroup
+
+    groupOwner = Column(String(255), ForeignKey('users.cognitoId'), nullable=False)
+    admins = relationship("User", secondary='group_admins_link')
+
+    # should only have members or children groups
+    members = relationship("User", secondary='group_members_link')
+    childrenGroups = relationship("Group", cascade="all, delete, delete-orphan")
+
+    def __init__(self, name, groupOwner):
+        self.name = name
+        self.groupOwner = groupOwner
---
+++ resources/models/src/models/group_members_link.py
@@ -0,0 +1,13 @@
+from . import Base
+from sqlalchemy import Column, Integer, String
+
+# Availability class is used, so we must define it here or else it has to be definied by clients of the Group class,
+# and they don't necessarily know that they need to do this
+from .group import Group
+from .user import User
+
+
+class GroupMembersLink(Base):
+    __tablename__ = 'group_members_link'
+    groupId = Column(Integer, ForeignKey('group.id'), primary_key=True)
+    userId = Column(String(255), ForeignKey('user.cognitoId'), primary_key=True)
